e9a06812f3671e3f4485f870ec2f266a77908eaa
kushira46-jpg <kushira46@gmail.com>
2026-05-17 21:52:00 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file for Paryavaran-Kavalu.
+// Configuration here applies to all sub-projects and modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
